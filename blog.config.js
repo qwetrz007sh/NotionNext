2003035ad2ce7fdd7c8ed240468d664c7192aa78
qwetrz007sh <coffee55k1@gmail.com>
2024-08-18 10:53:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -172,7 +172,7 @@ const BLOG = {
   // 代码主题 @see https://github.com/PrismJS/prism-themes
   PRISM_THEME_PREFIX_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_PREFIX_PATH ||
-    'https://raw.githubusercontent.com/PrismJS/prism-themes/master/themes/prism-vsc-dark-plus.css', // 代码块默认主题
+    'https://npm.elemecdn.com/prism-themes/themes/prism-vsc-dark-plus.css', // 代码块默认主题
   PRISM_THEME_SWITCH: process.env.NEXT_PUBLIC_PRISM_THEME_SWITCH || true, // 是否开启浅色/深色模式代码主题切换； 开启后将显示以下两个主题
   PRISM_THEME_LIGHT_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_LIGHT_PATH ||
